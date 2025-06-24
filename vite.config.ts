221dd4144834ba87134c8eec7e2ba5ebdf5367ce
reneTechnoExponent <rene@technoexponent.com>
2025-06-24 15:57:59 -0400

    feat: Add generated code

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as react from '@vitejs/plugin-react'
 import path from 'path'
 import { apiServerPlugin } from './api/vite-server.js';
 
 export default defineConfig({
-  plugins: [react(), apiServerPlugin()],
+  plugins: [(react as any).default(), apiServerPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
